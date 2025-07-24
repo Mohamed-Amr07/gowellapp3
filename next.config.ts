29adc5c171cf25d81848f93ede68be9123d12fc1
Mohamed-Amr07 <121493339+Mohamed-Amr07@users.noreply.github.com>
2025-07-24 23:47:55 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/gowellapp2',
-  assetPrefix: '/gowellapp2/',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
